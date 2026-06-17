cf0e2381d20c42a20fda942d3b82792bfa756155
AadarshJena <aadarsh.jena2008@gmail.com>
2026-06-17 16:15:11 -0400

    Retried Kadane's Algorithm

--- KadanesAlgorithm.py
+++ KadanesAlgorithm.py
@@ -74,4 +74,21 @@ class BetterSolution:
         print(max_sum)
     
 gang = BetterSolution()
-gang.maxSubarraySum([-1,-3,-4])
+gang.maxSubarraySum([-1,-3,-4]) 
+
+
+
+
+class RecreatingBetterSolution:
+    def maxSubarraySum(self, arr):
+        curr_sum = 0
+        max_sum = float('-inf')
+        
+        for i in range(len(arr)):
+            curr_sum = curr_sum + arr[i]
+            max_sum = max(max_sum, curr_sum)
+            
+            if curr_sum < 0:
+                curr_sum = 0
+                
+        print(max_sum)
