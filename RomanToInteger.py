8a8ac9723bde49dc50d7a1eafed3ff2a5f06cd85
AadarshJena <aadarsh.jena2008@gmail.com>
2026-06-17 18:19:19 -0400

    Solved 2 Leetcode Problems
    Used Claude for LongestSubstring after pulling my hair out for an hour trying to solve it.
    
    Solved RomanToInteger by myself with no help; Runtime was slow asf but memory was above average!

---
+++ RomanToInteger.py
@@ -0,0 +1,31 @@
+#SOLVED ALL BY MYSELF
+#Leetcode Easy :(
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        operators = "IXC"
+        dict = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        sum = 0
+        next_letter = "I"
+        i = 0
+        
+        while i < len(s):
+            if i == len(s) -1:
+                sum = sum + dict[s[i]]
+                return sum
+            else:
+                next_letter = s[i+1]
+                if s[i] in operators and dict[next_letter] > dict[s[i]]:
+                    next_letter = s[i+1]
+                    sum = sum + (dict[next_letter] - dict[s[i]])
+                    i = i + 2
+                else:
+                    sum = sum + dict[s[i]]
+                    i+=1
+        
+        return sum
+            
+                    
+gang = Solution()
+print(gang.romanToInt("MCMXCIV"))
+            
